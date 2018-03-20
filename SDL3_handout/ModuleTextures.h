968bbd1f9d192671cb6f76aee9371bc780e43ec6
sherzock <jacobo.galofre@gmail.com>
2018-03-20 10:32:17 +0100

    rqweqweqe

---
+++ SDL3_handout/ModuleTextures.h
@@ -0,0 +1,27 @@
+#ifndef __ModuleTextures_H__
+#define __ModuleTextures_H__
+
+#include "Module.h"
+#include "Globals.h"
+
+#define MAX_TEXTURES 50
+
+struct SDL_Texture;
+struct SDL_Surface;
+
+class ModuleTextures : public Module
+{
+public:
+	ModuleTextures();
+	~ModuleTextures();
+
+	bool Init();
+	bool CleanUp();
+
+	SDL_Texture* const Load(const char* path);
+	SDL_Surface* gsurface;
+public:
+	SDL_Texture * textures[MAX_TEXTURES] = {nullptr};
+};
+
+#endif // __ModuleTextures_H__
